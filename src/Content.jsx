25dc4cf3359fc69c33fe532019dbc2612939066d
DMelnikk <101388153+DMelnikk@users.noreply.github.com>
2023-03-29 11:00:31 +0300

    layout is done (#2)

---
+++ src/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <main>
+            <h2>Добро пожаловать!</h2>
+            <p>Это мой сайт. Здесь вы найдете много интересной информации.</p>
+        </main>
+    );
+}
+
+export default Content;
